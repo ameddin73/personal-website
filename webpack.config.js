d91bd05930670429da5dfa09cc5a9f4096199db8
almed4 <alexandre.meddin@ingka.ikea.com>
2021-11-26 22:34:41 -0500

    basic css

--- webpack.config.js
+++ webpack.config.js
@@ -23,8 +23,23 @@ module.exports = {
     module: {
         rules: [
             {
-                test: /\.css$/i,
-                use: ['style-loader', 'css-loader'],
+                test: /\.css$/,
+                use: [
+                    "style-loader",
+                    {
+                        loader: "css-loader",
+                        options: {
+                            importLoaders: 1,
+                            modules: true,
+                        },
+                    },
+                ],
+                include: /\.module\.css$/,
+            },
+            {
+                test: /\.css$/,
+                use: ["style-loader", "css-loader"],
+                exclude: /\.module\.css$/,
             },
             {
                 test: /\.html$/i,
