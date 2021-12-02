065b55689bda60d2a7e2fd5f657d158714e8ce6d
amedin73 <ameddin73@gmail.com>
2021-12-01 19:59:27 -0500

    extract animation from nav

--- src/index.js
+++ src/index.js
@@ -25,6 +25,10 @@ const pages = {
         title: 'Contact'
     },
 }
+const contentClasses = {
+    hidden: 'cont_hidden',
+    animate: 'cont_animate'
+}
 
 /**
  * Main
@@ -40,32 +44,81 @@ $(function () {
     if (page === undefined) page = pages.about;
 
     // Navigate to path on load
-    navigate({
-        page,
-        search: window.location.search
-    });
+    if ($('#top-nav').css('display') === 'none') {
+        navigateMobile({
+            page,
+            search: window.location.search
+        });
+    } else {
+        navigateDesktop({
+            page,
+            search: window.location.search
+        });
+    }
 
     // Routes for nav bar
-    $('.nav_btn').on('click', null, null, function () {
+    $('.top-nav_btn').on('click', null, null, function () {
         // Don't do anything if it's already selected
-        if ($(this).hasClass('nav_btn_selected')) return;
-        const id = $(this).attr('id');
-        navigate({page: pages[id]})
+        if ($(this).hasClass('top-nav_btn_selected')) return;
+        const id = $(this).attr('value');
+        navigateDesktop({page: pages[id]})
     });
 });
 
+// Trigger nav and update graphics for mobile dimension
+function navigateMobile({page: {id, path, title}, search}) {
+    // Update path + shared stuff
+    navigate({
+        path, title, search,
+        bgColor: 'blue'
+    });
+}
+
+// Trigger nav and update graphics for desktop dimension
+function navigateDesktop({page: {id, path, title}, search}) {
+    const content = $('#page_content');
+    const button = $(`#top-nav_${id}`);
+    const customCss = $("head").children(':last');
+
+    // Scroll bg
+    // Get starting width (inherited from button)
+    // create animated keyfraes for expand
+    customCss.html(`
+    .cont_visible {
+        transform: translateX(${button[0].getBoundingClientRect().x}px)
+            scale(${button[0].getBoundingClientRect().width / $(window).width()}, 0);
+    }
+    @keyframes expand {
+        from {
+            transform: translateX(${button[0].getBoundingClientRect().x}px)
+                scale(${button[0].getBoundingClientRect().width / $(window).width()}, 0);
+        }
+        to {
+            transform: translateX(0) scale(1, 1);
+        }
+    `);
+
+    // Update selected button and add new content
+    $('.top-nav_btn').removeClass('top-nav_btn_selected');
+    button.addClass('top-nav_btn_selected')
+
+    // Update path + shared stuff
+    navigate({
+        path, title, search,
+        bgColor: button.css('background-color')
+    });
+
+    // Update top-nav color
+    $('#top-nav').css('background-color', content.css('background-color'))
+}
+
 // Push a new path and update document metadata
-function navigate({page: {id, path, title}, search}) {
+function navigate({path, title, search, bgColor}) {
     const content = $('#page_content');
     const background = $('#page_bg');
-    const classes = {
-        hidden: 'cont_hidden',
-        start: 'cont_start',
-        visible: 'cont_visible',
-        animate: 'cont_animate'
-    }
-    const button = $(`#${id}`);
-    const customCss = $("head").children(':last');
+
+    // Strip animation
+    content.removeClass(contentClasses.animate);
 
     // Update path
     window.history.pushState(window.history.state,
@@ -76,28 +129,19 @@ function navigate({page: {id, path, title}, search}) {
 
     // Update content
     // Shift to bg
-    $('.nav_btn').removeClass('nav_btn_selected');
     background.css('background-color', content.css('background-color'))
     background.html(content.innerHTML);
 
     // Strip styling
     content.empty();
-    content.removeClass(`${classes.visible} ${classes.animate}`);
-    content.addClass('cont_hidden');
+    content.addClass(contentClasses.hidden);
 
-    // Update selected button and add new content
-    button.addClass('nav_btn_selected')
+    content.css('background-color', bgColor)
+
+    // Add styling
+    content.removeClass(contentClasses.hidden);
+    content.addClass(contentClasses.animate);
+
+    // Load new content
     content.load(`pages${path}/index.html`);
-    content.css('background-color', button.css('background-color'))
-
-    // Scroll bg
-    // Get starting width (inherited from button)
-    customCss.html(`.${classes.visible} {width: ${button[0].getBoundingClientRect().width}px;\
-    left: ${button[0].getBoundingClientRect().x}px}`);
-    content.addClass(classes.visible);
-    content.removeClass(classes.hidden)
-    content.addClass(classes.animate);
-
-    // Update nav color
-    $('#nav').css('background-color', content.css('background-color'))
 }
