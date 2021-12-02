db6e64ad4bcbe0f91540d1306b098db9eb50813e
amedin73 <ameddin73@gmail.com>
2021-12-01 19:59:26 -0500

    convert to status and add routing

---
+++ serve.js
@@ -0,0 +1,10 @@
+const browserSync = require("browser-sync").create();
+const history = require('connect-history-api-fallback');
+
+browserSync.init({
+    files: ["./*"],
+    server: {
+        baseDir: ".",
+        middleware: [ history() ]
+    }
+});
