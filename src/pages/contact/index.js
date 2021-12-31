b1d0c4116ba1954c44a9e3fc3024480b3648d1a0
amedin73 <ameddin73@gmail.com>
2021-12-30 18:23:46 -0800

    animate form in and out. form complete for contact

--- src/pages/contact/index.js
+++ src/pages/contact/index.js
@@ -18,15 +18,14 @@ $(function () {
     $messageTextArea.on('keyup', function () {
         if (initialHeight < $(this).prop('scrollHeight')) {
             // Don't grow once we're fullscreen
-            console.log(`${$(this).prop('scrollHeight')} < ${$(this).prop('clientHeight')}`)
             const clientHeight = $(this).prop('clientHeight');
             const currentHeight = $(this).css('height');
             const notMax = $contactContainer.prop('scrollHeight') <= $pageContent.prop('scrollHeight') - 64;
             $(this).css('height', '1px');
-            if ($(this).prop('scrollHeight') <= clientHeight || (notMax
+            if ($(this).prop('scrollHeight') < clientHeight || (notMax
                 && $(this).prop('scrollHeight') > clientHeight)) {
                 $(this).css('height', `${$(this).prop('scrollHeight')}px`);
-            } else if ($(this).prop('scrollHeight') > clientHeight) {
+            } else {
                 $(this).css('height', currentHeight);
             }
         }
@@ -34,7 +33,25 @@ $(function () {
 });
 
 function submitSuccess() {
-    $('#contact_form').hide();
+    const customCss = $("head").children(':last');
+    $('#contact_form').addClass('form_sent');
+    const $header = $('#contact_header');
+
+    // Center header
+    customCss.html(`
+    .header_center {
+        transform: translateY(${$(window).height() / 2 - $header.position().top - ($header.height() * 2)}px) !important;
+    }
+    `)
+
+    $header.removeClass('appear_message');
+    $header.addClass('header_sent');
+    setTimeout(function () {
+        $header.addClass('header_center');
+    }, 10);
+    setTimeout(function () {
+        $header.text('Message sent.');
+    }, 450);
 }
 
 function submitFailure() {
