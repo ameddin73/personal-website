0b28e184f6bb37874543f184d9970770b60b424e
amedin73 <ameddin73@gmail.com>
2022-01-03 19:40:12 -0800

    add personal site and some animations to help guide users to open projects

--- src/pages/projects/index.js
+++ src/pages/projects/index.js
@@ -2,7 +2,8 @@ $(function () {
     /* open active project */
     const projectSelected = 'project_selected';
     const fuzzyImage = 'project_graphic_fuzzy';
-    $('.project_container').on('click', function () {
+    const $projectContainer = $('.project_container');
+    $projectContainer.on('click', function () {
         const $content = $(this).find('.project_content');
         const $images = $(this).find('img');
 
@@ -11,11 +12,22 @@ $(function () {
 
         $('.project_content').removeClass(projectSelected);
         $('img').removeClass(fuzzyImage);
+        $('.project_chevron').removeClass('flipped_chevron');
 
         if (addClass) {
             $content.addClass(projectSelected);
             $images.addClass(fuzzyImage);
+            $(this).find('.project_chevron').addClass('flipped_chevron');
         }
+    });
+
+    $projectContainer.hover(function () {
+        console.log($(this).find('.project_graphic_mobile'))
+        $(this).find('.project_graphic_mobile').addClass('project_graphic_up');
+        $(this).find('.project_graphic_desktop').addClass('project_graphic_down');
+    }, function () {
+        $(this).find('.project_graphic_mobile').removeClass('project_graphic_up');
+        $(this).find('.project_graphic_desktop').removeClass('project_graphic_down');
     });
 
     /* prevent link clickthrough */
