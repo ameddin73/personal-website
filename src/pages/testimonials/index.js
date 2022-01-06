fd99e0de0fa117100feb4f04192762fc2ffceffa
amedin73 <ameddin73@gmail.com>
2022-01-06 07:12:38 -0800

    fix testimonials click propogation, page width, and smooth scroll

--- src/pages/testimonials/index.js
+++ src/pages/testimonials/index.js
@@ -1,4 +1,8 @@
 $(function () {
+    const $page = $('.page_content');
+    const $spotlight = $('.testimonial_spotlight');
+    const $box = $('.testimonial_box');
+
     /* check for hash */
     if (window.location.hash) {
         window.location.replace(window.location.hash);
@@ -6,22 +10,27 @@ $(function () {
 
     /* close spotlight */
     $(window).on('click', function () {
-        if ($('.testimonial_spotlight').css('display') !== 'none') {
+        if (window.location.hash) {
             window.location.replace("#");
             $('#testimonial_spotlight_container').removeClass('testimonial_spotlight_pointer_events')
         }
     });
-
-    $('.testimonial_spotlight').click(function (event) {
+    $spotlight.on('click', function (event) {
         event.stopPropagation();
     });
 
-    $('.testimonial_spotlight').on('target', function () {
+    $spotlight.on('target', function () {
         $('#testimonial_spotlight_container').addClass('testimonial_spotlight_pointer_events')
     })
 
+    $box.on('click', function () {
+        $page.animate({
+            scrollTop: 0
+        }, 500);
+    });
+
     /* animate in */
-    $('.testimonial_box').each(function (index) {
+    $box.each(function (index) {
         $(this).css('animation-delay', `${index * 75}ms`);
     });
 });
