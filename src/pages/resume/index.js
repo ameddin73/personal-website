2d90714e56da6ddb058438b275271b6ea9452735
Alex Meddin <ameddin73@gmail.com>
2023-03-01 11:59:40 -0800

    feat: add pronouns, fix: skill formatting

--- src/pages/resume/index.js
+++ src/pages/resume/index.js
@@ -17,20 +17,64 @@ $(function() {
   });
 
   // Remove list wrapped dividers
-  const wList = [];
-  const wrapped = $("li.resume_stack_entry")
-  wrapped.each(function() { wList.push($(this)) })
-  for (let i = 1; i < wList.length; i++) {
-    if (i == 0) {
-      return;
-    }
-    if (wList[i].offset().left < wList[i - 1].offset().left) {
-      wList[i].css('border-left', 'none')
-      wList[i].css('padding-left', '0')
-      wList[i].css('text-align', 'left')
+  // var wList = [];
+  // var wrapped = $("li.resume_stack_entry");
+  // resizeQueue = [];
 
-      wList[i - 1].css('text-align', 'right')
-      wList[i - 1].css('padding-right', '0')
+  // var lock = false;
+  // async function workQueue() {
+  //   if (lock) return;
+  //   lock = true
+  //   while (resizeQueue.length > 0) {
+  //     index = resizeQueue.shift();
+  //     resizeSkills(index);
+  //   }
+  //   lock = false;
+  // }
+
+  // wrapped.each(function(index) {
+  //   wList.push($(this));
+  //   $(this).onPositionChanged(function(index) {
+  //     resizeQueue.forEach(function(v) {
+  //       if (v < index) return;
+  //     })
+  //     const i = resizeQueue.indexOf(index);
+  //     if (i > -1) {
+  //       resizeQueue.splice(i, 1)
+  //     };
+  //     resizeQueue.push(index);
+  //   }, index);
+  // })
+
+  function formatSkills() {
+    var wList = [];
+    var wrapped = $("li.resume_stack_entry")
+    wrapped.each(function() { wList.push($(this)) })
+    for (let i = 1; i < wList.length; i++) {
+      wList = []
+      wrapped.each(function() { wList.push($(this)) })
+      if (wList[i].offset().left < wList[i - 1].offset().left) {
+        // console.log(wList[i].text());
+        // console.log(wList[i].offset());
+        // console.log(wList[i - 1].text());
+        // console.log(wList[i - 1].offset());
+        wList[i].css('border-left', 'none');
+        wList[i].css('padding-left', '0');
+        wList[i].css('text-align', 'left');
+
+        wList[i - 1].css('text-align', 'right');
+        wList[i - 1].css('padding-right', '0');
+      } else {
+        wList[i].css('border-left', '');
+        wList[i].css('padding-left', '');
+        wList[i].css('text-align', '');
+
+        wList[i - 1].css('text-align', '');
+        wList[i - 1].css('padding-right', '');
+      }
     }
   }
+
+  formatSkills();
+  setTimeout(formatSkills, 1000)
 });
