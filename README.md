440a2939a11fbe7601fc748f7524aec0aa79beed
Alex Meddin <ameddin73@gmail.com>
2023-03-02 12:50:38 -0800

    feat: custom resume script

--- README.md
+++ README.md
@@ -11,7 +11,15 @@ Automagical gitops Netlify don't think about it. Print the resume first tho.
 ### Print Resume
 
 1. `alias chrome=<path-to-chrome>`
-1. `npm print`
+1. `npm start`
+1. `npm run print`
+
+#### Print Custom Resume
+
+1. `alias chrome=<path-to-chrome>`
+1. `npm run customize -- "goal" '"skill1","skill2"'`
+1. `npm start`
+1. `npm run print-special`
 
 ## Resources
 
--- src/pages/resume/index.js
+++ src/pages/resume/index.js
@@ -1,6 +1,4 @@
 $(function() {
-  // Add bonus stuff
-  $("#resume_goal").load("pages/resume/customizer.html");
 
   // Animate in
   $("h1, h2, h3, h4, a, p, li, span").each(function(i) {
@@ -19,34 +17,36 @@ $(function() {
     }, 1000);
   });
 
-  function formatSkills() {
-    var wList = [];
+  formatSkills();
+  // Add bonus stuff
+});
+
+function formatSkills() {
+  var wList = [];
+  var wrapped = $("li.resume_stack_entry")
+  wrapped.each(function() { wList.push($(this)) })
+  for (let i = 0; i < wList.length - 1; i++) {
+    wList = []
     var wrapped = $("li.resume_stack_entry")
     wrapped.each(function() { wList.push($(this)) })
-    for (let i = 1; i < wList.length; i++) {
-      wList = []
-      wrapped.each(function() { wList.push($(this)) })
-      if (wList[i].offset().left < wList[i - 1].offset().left) {
-        // console.log(wList[i].text());
-        // console.log(wList[i].offset());
-        // console.log(wList[i - 1].text());
-        // console.log(wList[i - 1].offset());
-        wList[i].css('border-left', 'none');
-        wList[i].css('padding-left', '0');
-        wList[i].css('text-align', 'left');
+    if (wList[i + 1].position().left < wList[i].position().left) {
+      console.log(wList[i].text());
+      console.log(wList[i].position());
+      console.log(wList[i + 1].text());
+      console.log(wList[i + 1].position());
+      wList[i + 1].css('border-left', 'none');
+      wList[i + 1].css('padding-left', '0');
+      wList[i + 1].css('text-align', 'left');
 
-        wList[i - 1].css('text-align', 'right');
-        wList[i - 1].css('padding-right', '0');
-      } else {
-        wList[i].css('border-left', '');
-        wList[i].css('padding-left', '');
-        wList[i].css('text-align', '');
+      wList[i].css('text-align', 'right');
+      wList[i].css('padding-right', '10px');
+    } else {
+      wList[i + 1].css('border-left', '');
+      wList[i + 1].css('padding-left', '');
+      wList[i + 1].css('text-align', '');
 
-        wList[i - 1].css('text-align', '');
-        wList[i - 1].css('padding-right', '');
-      }
+      wList[i].css('text-align', '');
+      wList[i].css('padding-right', '');
     }
   }
-
-  formatSkills();
-});
+}
